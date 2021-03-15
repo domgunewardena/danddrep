54e44ee6836970f2e86b5bc8629a3210680c3829
domgunewardena <domgunewardena@gmail.com>
2021-03-15 07:17:37 +0000

    Create delete old reviews command

---
+++ rep_app/management/commands/delete_old_reviews.py
@@ -0,0 +1,36 @@
+from datetime import date, timedelta
+
+from django.core.management.base import BaseCommand, CommandError
+from rep_app.models import Review, Restaurant
+from rep_app.scrapers import Reviews
+from rep_app.restaurant_list import restaurant_dict
+
+class Command(BaseCommand):
+    
+    help = 'Create review objects from all reviews in database'
+    
+    def handle(self, *args, **options):
+        
+        def monday(date):
+            return date - timedelta(date.weekday())
+
+        def months_ago(date, n):
+            return date - timedelta(28*n)
+
+        def one_year_ago(date):
+            return date - timedelta(364)
+
+        today = one_year_ago(date.today())
+        upper_bound = months_ago(monday(today), 4)
+        
+        try:
+            old_reviews = Review.objects.filter(date__lt = upper_bound)
+            old_reviews.delete()
+            
+        except Exception as err:
+            self.stdout.write(self.style.ERROR('Error when deleting old reviews: ' + str(err)))
+            return
+        
+        self.stdout.write(self.style.SUCCESS('Old reviews deleted'))
+        
+        return

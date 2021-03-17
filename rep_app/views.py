6adcd50149ac5dfb3f1bda08db74c524da3bf5bc
domgunewardena <domgunewardena@gmail.com>
2021-03-17 11:49:02 +0000

    Remove registration view

--- rep_app/views.py
+++ rep_app/views.py
@@ -156,33 +156,6 @@ def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('rep_app:home_page'))
 
-def register(request):
-
-    registered = False
-
-    if request.method == "POST":
-        user_form = UserForm(data=request.POST)
-
-        if user_form.is_valid():
-
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-
-            registered = True
-
-        else:
-            print(user_form.errors)
-    else:
-        user_form = UserForm()
-
-    context = {
-        'user_form':user_form,
-        'registered':registered
-    }
-
-    return render(request,'registration.html',context)
-
 def user_login(request):
 
     if request.method == 'POST':
